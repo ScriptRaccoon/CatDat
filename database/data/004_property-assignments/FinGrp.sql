d3ae87fb2c6b766d7b62cf49f2cb7d3cb3f14452
Script Raccoon <scriptraccoon@gmail.com>
2026-04-08 21:47:35 +0200

    add lemma about Hilbert's Hotel

--- database/data/004_property-assignments/FinGrp.sql
+++ database/data/004_property-assignments/FinGrp.sql
@@ -93,7 +93,7 @@ VALUES
 	'FinGrp',
 	'countable powers',
 	FALSE,
-	'Assume that the power $P := (C_2)^{\mathbb{N}}$ exists. Since products are associative and finite products exist, we conclude $P \cong C_2 \times P$. If $P$ has $n$ elements, this means $n = 2n$, i.e. $n = 0$, a contradiction.'
+	'If countable powers exist, then by <a href="/lemma/hilberts_hotel">Hilbert''s Hotel</a> there is some object $P$ with $P \cong C_2 \times P$. If $P$ has $n$ elements, this means $n = 2n$, i.e. $n = 0$, a contradiction.'
 ),
 (
 	'FinGrp',
