ed5f43392f8ec6076ed53dec430c49970f73ff36
Script Raccoon <scriptraccoon@gmail.com>
2026-04-11 22:31:10 +0200

    display usage of each lemma

--- src/routes/lemma/[id]/+page.server.ts
+++ src/routes/lemma/[id]/+page.server.ts
@@ -1,5 +1,5 @@
-import type { Lemma } from '$lib/commons/types'
-import { query } from '$lib/server/db'
+import type { CategoryShort, Lemma } from '$lib/commons/types'
+import { batch } from '$lib/server/db'
 import { render_nested_formulas } from '$lib/server/rendering'
 import { error } from '@sveltejs/kit'
 import sql from 'sql-template-tag'
@@ -7,15 +7,30 @@ import sql from 'sql-template-tag'
 export const load = async (event) => {
 	const id = event.params.id
 
-	const { rows: lemmas, err } = await query<Lemma>(sql`
-        SELECT title, claim, proof FROM lemmas WHERE id = ${id}
-    `)
+	const { results, err } = await batch<[Lemma, CategoryShort]>([
+		sql`
+        	SELECT title, claim, proof FROM lemmas WHERE id = ${id}
+    	`,
+		sql`
+			SELECT c.id, c.name
+			FROM categories c
+			WHERE EXISTS (
+				SELECT 1
+				FROM category_property_assignments cp
+				WHERE cp.category_id = c.id
+				AND cp.is_deduced = FALSE
+				AND cp.reason LIKE '%/lemma/' || ${id} || '%'
+			);
+		`,
+	])
 
 	if (err) error(500, 'Could not load lemma')
+
+	const [lemmas, categories] = results
 
 	if (!lemmas.length) error(404, 'Lemma not found')
 
 	const lemma = lemmas[0]
 
-	return render_nested_formulas({ lemma })
+	return render_nested_formulas({ lemma, categories })
 }
--- src/routes/lemma/[id]/+page.svelte
+++ src/routes/lemma/[id]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import CategoryList from '$components/CategoryList.svelte'
 	import MetaData from '$components/MetaData.svelte'
 	import SuggestionForm from '$components/SuggestionForm.svelte'
+
 	let { data } = $props()
 </script>
 
@@ -15,5 +17,11 @@
 <h3>Proof</h3>
 
 {@html data.lemma.proof}
+
+<h3>Usage</h3>
+
+<p class="hint">This lemma is referenced in the following categories:</p>
+
+<CategoryList categories={data.categories} />
 
 <SuggestionForm />
