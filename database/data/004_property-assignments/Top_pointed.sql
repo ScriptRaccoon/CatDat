3e87ca74d96b01929d77b14f8810e4a87c93e628
Script Raccoon <scriptraccoon@gmail.com>
2026-04-16 11:59:16 +0200

    show that Set_* and Top_* have cocartesian cofiltered limits

--- database/data/004_property-assignments/Top_pointed.sql
+++ database/data/004_property-assignments/Top_pointed.sql
@@ -79,6 +79,13 @@ VALUES
 ),
 (
 	'Top*',
+	'cocartesian cofiltered limits',
+	TRUE,
+	'We continue the proof for <a href="/category/Set*">$\mathbf{Set}_*$</a> by showing that the natural bijective map <br>$\alpha : X \vee \lim_i Y_i \to \lim_i (X \vee Y_i)$<br>
+	is open. It suffices to consider open sets of two types: (1) If $U \subseteq X$ is open, the $\alpha$-image of $U \vee \lim_i Y_i$ is $p_{i_0}^{-1}(U \vee Y_{i_0})$ for any chosen index $i_0$, hence open. (2) If $i$ is an index and $V_i \subseteq Y_i$ is open, then the $\alpha$-image of $X \vee (p_i^{-1}(V_i) \cap \lim_i Y_i)$ is $p_i^{-1}(X \vee V_i)$, hence open.'
+),
+(
+	'Top*',
 	'coregular',
 	TRUE,
 	'Regular monomorphisms coincide with the embeddings (see below). Since $\mathbf{Top}$ is coregular, they are stable under pushouts, and pushouts in $\mathbf{Top}_*$ are the same.'
