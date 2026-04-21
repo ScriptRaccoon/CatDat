c7648d5efb3024a77c4b3498de1c6d8db7ea1de8
Script Raccoon <scriptraccoon@gmail.com>
2026-04-21 12:46:20 +0200

    group seed into single transaction

--- databases/catdat/scripts/seed.ts
+++ databases/catdat/scripts/seed.ts
@@ -17,34 +17,38 @@ async function seed() {
 	const invalid_folder = subfolders.find((f) => !f.match(/^\d{3}_/))
 	if (invalid_folder) throw new Error(`Invalid folder name: ${invalid_folder}`)
 
-	for (const folder of subfolders) {
-		const folder_path = path.join(data_folder, folder)
-		const files = (await fs.readdir(folder_path, { withFileTypes: true }))
-			.filter((f) => f.isFile() && f.name.endsWith('.sql'))
-			.map((f) => path.join(folder_path, f.name))
-			.sort()
+	const tx = await db.transaction()
 
-		for (const file of files) {
-			const base = path.basename(file)
-			const is_valid = base?.match(/^[A-Za-z0-9_.,\-()]+$/)
-			if (!is_valid) {
-				throw new Error(`Invalid file name: ${base}`)
-			}
+	try {
+		for (const folder of subfolders) {
+			const folder_path = path.join(data_folder, folder)
+			const files = (await fs.readdir(folder_path, { withFileTypes: true }))
+				.filter((f) => f.isFile() && f.name.endsWith('.sql'))
+				.map((f) => path.join(folder_path, f.name))
+				.sort()
 
-			const tx = await db.transaction()
-			try {
-				const sql = await fs.readFile(file, 'utf8')
-
-				await tx.executeMultiple(sql)
-				await tx.commit()
+			for (const file of files) {
+				const base = path.basename(file)
+				const is_valid = base?.match(/^[A-Za-z0-9_.,\-()]+$/)
+				if (!is_valid) {
+					throw new Error(`Invalid file name: ${base}`)
+				}
 
 				console.info(`Seed: ${base}`)
-			} catch (err) {
-				console.error(`Failed to seed ${file}`, err)
-				await tx.rollback()
-				process.exit(1)
+
+				try {
+					const sql = await fs.readFile(file, 'utf8')
+					await tx.executeMultiple(sql)
+				} catch (err) {
+					throw new Error(`Seed failed in ${base}: ${String(err)}`)
+				}
 			}
 		}
+		await tx.commit()
+	} catch (err) {
+		await tx.rollback()
+		console.error(err)
+		process.exit(1)
 	}
 
 	console.info('Seeded all data')
