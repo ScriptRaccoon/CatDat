e7231a48462b8f6cece85cc9d4a20855cd4e9400
Script Raccoon <scriptraccoon@gmail.com>
2026-04-21 23:41:13 +0200

    support for conditional relations

--- databases/catdat/data/003_properties/001_relations.sql
+++ databases/catdat/data/003_properties/001_relations.sql
@@ -1,8 +1,8 @@
-INSERT INTO relations (relation, negation) VALUES
-('is', 'is not'),
-('is a', 'is not a'),
-('is an', 'is not an'),
-('has', 'does not have'),
-('has a', 'does not have a'),
-('has an', 'does not have an'),
-('satisfies', 'does not satisfy');
+INSERT INTO relations (relation, negation, conditional) VALUES
+('is', 'is not', 'would be'),
+('is a', 'is not a', 'would be a'),
+('is an', 'is not an', 'would be an'),
+('has', 'does not have', 'would have'),
+('has a', 'does not have a', 'would have a'),
+('has an', 'does not have an', 'would have an'),
+('satisfies', 'does not satisfy', 'would satisfy');
---
+++ databases/catdat/migrations/019_conditional-relation.sql
@@ -0,0 +1,1 @@
+ALTER TABLE relations ADD COLUMN conditional TEXT NOT NULL DEFAULT 'would be'; 
