fa6253bf371d620d39f4328286d8db3bf1e74f8d
Script Raccoon <scriptraccoon@gmail.com>
2026-08-08 04:32:02 +0200

    add tests for symmetric monoidal categories

---
+++ tests/symmetric_monoidal_categories.spec.ts
@@ -0,0 +1,82 @@
+import { test, expect } from '@playwright/test'
+
+// This test file does not cover all features of symmetric monoidal categories,
+// but only a selection. The other features are covered sufficiently by the
+// test cases for the other types of categorical structures.
+
+test('user can navigate to a symmetric monoidal category', async ({ page }) => {
+	await page.goto('/')
+
+	await page
+		.getByRole('link', {
+			name: 'symmetric monoidal categories',
+			exact: true
+		})
+		.first()
+		.click()
+
+	await expect(
+		page.getByRole('heading', {
+			name: 'List of symmetric monoidal categories',
+			exact: true
+		})
+	).toBeVisible()
+
+	await expect(page).toHaveURL('/symmetric_monoidal_category-list')
+
+	await page
+		.getByRole('link', {
+			name: 'symmetric monoidal category of abelian groups',
+			exact: true
+		})
+		.click()
+
+	await expect(
+		page.getByRole('heading', {
+			name: 'symmetric monoidal category of abelian groups',
+			exact: true
+		})
+	).toBeVisible()
+
+	await expect(page).toHaveURL('/symmetric_monoidal_category/Ab_tensor')
+})
+
+test('user can view symmetric monoidal category details', async ({ page }) => {
+	await page.goto('/symmetric_monoidal_category/Ab_tensor')
+
+	await expect(
+		page.getByRole('heading', {
+			name: 'symmetric monoidal category of abelian groups',
+			exact: true
+		})
+	).toBeVisible()
+
+	await expect(page.getByText('tensor product of abelian groups')).toBeVisible()
+	await expect(page.getByText('is closed')).toBeVisible()
+	await expect(page.getByText('is cocomplete')).toBeVisible()
+	await expect(page.getByText('is not strict')).toBeVisible()
+	await expect(page.getByText('is not cartesian')).toBeVisible()
+})
+
+test('user can navigate to the underlying category', async ({ page }) => {
+	await page.goto('/symmetric_monoidal_category/Set_cartesian', {
+		waitUntil: 'networkidle'
+	})
+
+	await page
+		.getByRole('link', {
+			name: 'category of sets',
+			exact: true
+		})
+		.first()
+		.click()
+
+	await expect(
+		page.getByRole('heading', {
+			name: 'category of sets',
+			exact: true
+		})
+	).toBeVisible()
+
+	await expect(page).toHaveURL('/category/Set')
+})
