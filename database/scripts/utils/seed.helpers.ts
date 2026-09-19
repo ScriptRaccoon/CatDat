0b34e2c57da94032cfc320bb046c6090a75e2faa
Script Raccoon <scriptraccoon@gmail.com>
2026-09-20 00:47:39 +0200

    validate seed files with new valibot schemas

--- database/scripts/utils/seed.helpers.ts
+++ database/scripts/utils/seed.helpers.ts
@@ -3,11 +3,12 @@ import path from 'node:path'
 import fs from 'node:fs'
 import YAML from 'yaml'
 import { devlog } from '$shared/utils'
-import { StructureYaml } from './seed.types'
+import * as v from 'valibot'
+import { structure_yaml_schema } from './seed.schemas'
 
-function read_yaml_file<T>(...parts: string[]): T {
+function read_yaml_file(...parts: string[]) {
 	const content = fs.readFileSync(path.join(...parts), 'utf8')
-	return YAML.parse(content) as T
+	return YAML.parse(content) as unknown
 }
 
 function get_yaml_files(folder: string) {
@@ -17,18 +18,30 @@ function get_yaml_files(folder: string) {
 		.sort()
 }
 
-export function seed_file<T>(
+export function seed_file<Schema extends v.GenericSchema>(
 	db: Database,
 	label: string,
 	file: string,
-	insert: (item: T) => void
+	schema: Schema,
+	insert: (item: v.InferOutput<Schema>) => void
 ) {
 	devlog(`\nSeed ${label} ...`)
-	const item = read_yaml_file<T>(file)
+	const item = read_yaml_file(file)
+
+	const { output, issues } = v.safeParse(schema, item)
+
+	if (issues) {
+		console.error(`❌ Contents of ${file} have invalid format`)
+		issues.forEach((issue) => {
+			const path = issue.path?.map((item) => item.key).join('.') ?? '<root>'
+			console.error(`${path}: ${issue.message}`)
+		})
+		process.exit(1)
+	}
 
 	const tx = db.transaction(() => {
 		db.pragma('defer_foreign_keys = ON')
-		insert(item)
+		insert(output)
 	})
 
 	try {
@@ -39,11 +52,12 @@ export function seed_file<T>(
 	}
 }
 
-export function seed_files<T>(
+export function seed_files<Schema extends v.GenericSchema>(
 	db: Database,
 	label: string,
 	folder: string,
-	insert: (item: T) => void
+	schema: Schema,
+	insert: (item: v.InferOutput<Schema>) => void
 ) {
 	devlog(`\nSeed ${label} ...`)
 
@@ -55,8 +69,20 @@ export function seed_files<T>(
 		for (const file of files) {
 			devlog(`Seed: ${file}`)
 
-			const item = read_yaml_file<T>(folder, file)
-			insert(item)
+			const item = read_yaml_file(folder, file)
+
+			const { output, issues } = v.safeParse(schema, item)
+
+			if (issues) {
+				console.error(`❌ Contents of ${file} have invalid format`)
+				issues.forEach((issue) => {
+					const path = issue.path?.map((item) => item.key).join('.') ?? '<root>'
+					console.error(`${path}: ${issue.message}`)
+				})
+				process.exit(1)
+			}
+
+			insert(output)
 		}
 	})
 
@@ -68,7 +94,9 @@ export function seed_files<T>(
 	}
 }
 
-export function get_property_assignments(structure: StructureYaml) {
+export function get_property_assignments(
+	structure: v.InferOutput<typeof structure_yaml_schema>
+) {
 	return [
 		...structure.satisfied_properties.map((entry) => ({
 			...entry,
